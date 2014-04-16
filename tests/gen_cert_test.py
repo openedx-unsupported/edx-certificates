1b49cd24435d6d1f4cb629dcb5d0b606e98354fc
John Jarvis <john@jarv.org>
2014-04-16 16:52:26 -0400

    PEP8

--- tests/gen_cert_test.py
+++ tests/gen_cert_test.py
@@ -2,7 +2,6 @@
 from gen_cert import CertificateGen
 from gen_cert import S3_CERT_PATH, S3_VERIFY_PATH
 from nose.tools import assert_true
-from nose.plugins.skip import SkipTest
 import settings
 import os
 import gnupg
@@ -13,6 +12,7 @@ from test_data import NAMES
 
 VERIFY_FILES = ['valid.html', 'Certificate.pdf.sig', 'verify.html']
 DOWNLOAD_FILES = ['Certificate.pdf']
+
 
 def test_cert_gen():
     """
@@ -27,24 +27,27 @@ def test_cert_gen():
         tmpdir = tempfile.mkdtemp()
         cert = CertificateGen(course_id)
         (download_uuid, verify_uuid, download_url) = cert.create_and_upload(
-                        'John Smith', upload=False, copy_to_webroot=True,
-                        cert_web_root=tmpdir, cleanup=True)
+            'John Smith', upload=False, copy_to_webroot=True,
+            cert_web_root=tmpdir, cleanup=True)
 
         verify_files = os.listdir(
-                os.path.join(tmpdir, S3_VERIFY_PATH, verify_uuid))
+            os.path.join(tmpdir, S3_VERIFY_PATH, verify_uuid))
         download_files = os.listdir(
-                os.path.join(tmpdir, S3_CERT_PATH, download_uuid))
-
+            os.path.join(tmpdir, S3_CERT_PATH, download_uuid))
 
         # Verify that all files are generated
         assert_true(set(verify_files) == set(VERIFY_FILES))
         assert_true(set(download_files) == set(DOWNLOAD_FILES))
 
         # Verify that the detached signature is valid
-        pdf = os.path.join(tmpdir,
-                S3_CERT_PATH, download_uuid, 'Certificate.pdf')
-        sig = os.path.join(tmpdir,
-                S3_VERIFY_PATH, verify_uuid, 'Certificate.pdf.sig')
+        pdf = os.path.join(
+            tmpdir,
+            S3_CERT_PATH, download_uuid, 'Certificate.pdf'
+        )
+        sig = os.path.join(
+            tmpdir,
+            S3_VERIFY_PATH, verify_uuid, 'Certificate.pdf.sig'
+        )
         gpg = gnupg.GPG(gnupghome=settings.CERT_GPG_DIR)
 
         with open(sig) as f:
@@ -63,10 +66,12 @@ def test_cert_upload():
     downloaded via http
     """
 
-    cert = CertificateGen(settings.CERT_DATA.keys()[0], settings.CERT_AWS_ID,
-                                settings.CERT_AWS_KEY)
+    cert = CertificateGen(
+        settings.CERT_DATA.keys()[0], settings.CERT_AWS_ID,
+        settings.CERT_AWS_KEY
+    )
     (download_uuid, verify_uuid, download_url) = cert.create_and_upload(
-                      'John Smith')
+        'John Smith')
     r = urllib2.urlopen(download_url)
     with tempfile.NamedTemporaryFile(delete=True) as f:
         f.write(r.read())
@@ -82,4 +87,4 @@ def test_cert_names():
         for name in NAMES:
             cert = CertificateGen(course_id)
             (download_uuid, verify_uuid, download_url) = cert.create_and_upload(
-                            name, upload=False)
+                name, upload=False)
