607eb09a787fe210298bb2984f50b203b466c9d7
Joe Blaylock <jrbl@jrbl.org>
2014-05-21 13:19:17 -0700

    WIP

--- settings.py
+++ settings.py
@@ -125,5 +125,5 @@ if os.path.isfile(ENV_ROOT / "auth.json"):
 
 TEMPLATE_DIR = os.path.join((CERT_PRIVATE_DIR, TEMPLATE_DATA_SUBDIR))
 
-with open(CERT_PRIVATE_DIR / CERT_DATA_FILE) as f:
+with open(os.path.join((CERT_PRIVATE_DIR, CERT_DATA_FILE))) as f:
     CERT_DATA = yaml.load(f.read())
