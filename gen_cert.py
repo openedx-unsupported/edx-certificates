9bfe2fa637a737cc32aed47aef3646610b93d458
Victoria <mimi@braniac>
2015-09-15 15:47:01 -0300

    Espaciado de línea

--- gen_cert.py
+++ gen_cert.py
@@ -574,25 +574,31 @@ class CertificateGen(object):
             styleOpenSans.leading = 21
         elif '4.01x' in self.course:
             styleOpenSans.fontSize = 20
-            styleOpenSans.leading = 10
+            #styleOpenSans.leading = 10
+            styleOpenSans.leading = 21
         elif 'Stat2.1x' in self.course:
             styleOpenSans.fontSize = 20
-            styleOpenSans.leading = 10
+            #styleOpenSans.leading = 10
+            styleOpenSans.leading = 21
         elif 'CS191x' in self.course:
             styleOpenSans.fontSize = 20
-            styleOpenSans.leading = 10
+            #styleOpenSans.leading = 10
+            styleOpenSans.leading = 21
         elif '6.00x' in self.course:
             styleOpenSans.fontSize = 20
             styleOpenSans.leading = 21
         elif 'PH278x' in self.course:
             styleOpenSans.fontSize = 20
-            styleOpenSans.leading = 10
+            #styleOpenSans.leading = 10
+            styleOpenSans.leading = 21
         else:
             styleOpenSans.fontSize = 24
-            styleOpenSans.leading = 10
+            #styleOpenSans.leading = 10
+            styleOpenSans.leading = 21
         styleOpenSans.textColor = colors.Color(
             0, 0.624, 0.886)
         styleOpenSans.alignment = TA_CENTER
+        styleOpenSans.leading = 21
 
         paragraph_string = u"<b style='line-height: 2'><i>{0}: {1}</i></b>".format(
             self.course, self.long_course.decode('utf-8'))
