521b939387ad23459fe9766c0f094eb9f0a82ce2
Aaron Zhao <rudy710@qq.com>
2018-10-25 14:35:23 +0800

    change pdf template

--- gen_cert.py
+++ gen_cert.py
@@ -253,7 +253,7 @@ class CertificateGen(object):
         # Else, the filename is calculated from the version and course_id.
         template_pdf = cert_data.get('TEMPLATEFILE', template_pdf)
         template_prefix = '{0}/v{1}-cert-templates'.format(TEMPLATE_DIR, self.template_version)
-        template_pdf_filename = "{0}/certificate-template-{1}-{2}.pdf".format(template_prefix, self.org, self.course)
+        template_pdf_filename = "{0}/certificate-template-{1}.pdf".format(template_prefix, self.org)
         if template_pdf:
             template_pdf_filename = "{0}/{1}".format(template_prefix, template_pdf)
             if 'verified' in template_pdf:
